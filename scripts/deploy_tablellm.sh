cfa2f3b08efc9679ffee655a4bdb5121f629d54b
ranjith-shodhai <ranjith@shodh.ai>
2024-09-04 21:14:30 +0530

    Update deploy_tablellm.sh

--- scripts/deploy_tablellm.sh
+++ scripts/deploy_tablellm.sh
@@ -1,7 +1,7 @@
 export CUDA_VISIBLE_DEVICES=0,1,2,3
 
 PORT=8750
-MODEL_PATH=/root/host/TableLLM-13b
+MODEL_PATH=/host/TableLLM-13b
 
 python -m vllm.entrypoints.api_server \
     --model $MODEL_PATH \
