32f6e2de6334c0d64aaa09b00f6f243a4fc24a75
ranjith-shodhai <ranjith@shodh.ai>
2024-09-04 20:36:19 +0530

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Update the package list
+apt-get update
+
+apt install sudo 
+# Install git-lfs using a script from packagecloud
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
+
+sudo apt-get install git-lfs
+
+git clone https://huggingface.co/RUCKBReasoning/TableLLM-13b
+
+pip install -r requirements.txt 
+
+bash scripts/deploy_tablellm.sh
+
+
+
+
